7b7b11a6014e3e25ed7e419e57425f719aa21020
Matthew <mleonard36@gatech.edu>
2018-06-11 09:36:24 -0400

    Added logout button to success page

--- src/SuccessLogin.java
+++ src/SuccessLogin.java
@@ -1,2 +1,16 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class SuccessLogin {
+
+    @FXML Button logoutButton;
+
+    @FXML
+    public void logoutButtonHandler(ActionEvent event) throws IOException {
+        logoutButton.getScene().setRoot(FXMLLoader.load(getClass().getResource("/welcome/welcomeScreen.fxml")));
+    }
 }
