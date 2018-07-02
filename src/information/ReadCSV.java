4d413bf3d79bddd18aa151351f1f1b2504c0c6c2
rpend <rpendley@gmail.com>
2018-07-02 19:23:43 -0400

    made it so that bus's stop field is initialized with first stop

--- src/information/ReadCSV.java
+++ src/information/ReadCSV.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 
 public class ReadCSV {
 
-    public static String CSVPATH = "C:\\Users\\Matthew\\Desktop\\TeamDelta Marta\\src\\information\\MARTA Simulation All Data.csv";
+    public static String CSVPATH = "C:\\Users\\rpend\\TeamDelta-marta\\src\\information\\MARTA Simulation All Data.csv";
 
 
     public static ArrayList<Bus> getBuses() {
@@ -34,6 +34,11 @@ public class ReadCSV {
                     int riders = Integer.parseInt(data[4]);
                     int speed = Integer.parseInt(data[6]);
                     int stop = -1;
+                    for (Route r: getRoutes()) {
+                        if (r.getId() == route) {
+                            stop = r.getPath()[0].getId();
+                        }
+                    }
                     int stopCount = 0; //intitializes buses at beginning of route
                     Bus newBus = new Bus(id, route, location, riders, speed, stop, stopCount);
                     busArray.add(newBus);
