d0b6322466388772d9480e1f39502675923ba3ec
Matthew <mleonard36@gatech.edu>
2018-07-01 22:05:40 -0400

    Updated BusInfo Controller and fixed randomization

--- src/information/bus/BusInfo.java
+++ src/information/bus/BusInfo.java
@@ -10,6 +10,13 @@ public class BusInfo {
     @FXML Label locationLabel;
     @FXML Label riderLabel;
     @FXML Label speedLabel;
+    @FXML Label currentStopLabel;
+    @FXML Label nextStopLabel;
+    @FXML Label distanceToNextLabel;
+    @FXML Label timeUntilNextLabel;
+    @FXML Label numberExitingLabel;
+    @FXML Label numberBoardingLabel;
+    @FXML Label updatedCountLabel;
 
     public void initData(Bus pick) {
         idLabel.setText(String.valueOf(pick.getId()));
@@ -17,6 +24,10 @@ public class BusInfo {
         locationLabel.setText(String.valueOf(pick.getLocation()));
         riderLabel.setText(String.valueOf(pick.getRiders()));
         speedLabel.setText(String.valueOf(pick.getSpeed()));
+
+        numberExitingLabel.setText(String.valueOf(pick.getNextExit()));
+        numberBoardingLabel.setText(String.valueOf(pick.getNextBoard()));
+        updatedCountLabel.setText(String.valueOf(pick.getRiders() - pick.getNextExit() + pick.getNextBoard()));
     }
 
 }
